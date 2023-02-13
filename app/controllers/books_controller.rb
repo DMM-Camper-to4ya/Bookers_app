eb00a1c4e7a71518170713fcb04d8790a95de577
DMM-Camper-to4ya <to4ya.oockv36oo@gmail.com>
2023-02-13 07:25:36 +0000

    in the middle

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+
   def new
     #viewへ渡す為のインスタンス変数に空のModelオブジェクトを生成する。
     @book=Book.new
@@ -6,15 +7,21 @@ class BooksController < ApplicationController
 
   def create
   #データを受け取り新規登録するためのインスタンス作成
-  book=Book.new(book_params)
+  @book=Book.new(book_params)
   #データをデータベースに保存するためのsaveメソッド実行
-  book.save
+  if @book.save
   #Top画面へリダイレクト
   redirect_to book_path(book.id)
+  flash[:notice] = "Book was succesfully created."
+  else
+  @books=Book.all
+  render:index
+  end
   end
 
   def index
   @books = Book.all
+  @book =Book.new
   end
 
 
@@ -37,6 +44,7 @@ class BooksController < ApplicationController
   book=Book.find(params[:id])
   book.destroy
   redirect_to books_path
+  flash[:notice] = "Book was succesfully destroyed."
  end
 
 
